b2e02a4c2d71c1b324d5c05e667a62b4d7dad3c3
jagatkhatri01 <jagatkhatri383@gmail.com>
2024-01-14 19:01:43 +0545

    added register

--- NoticeBoard/NoticeBoard/urls.py
+++ NoticeBoard/NoticeBoard/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('notices/', include('notices.urls')),
+    path('', include('accounts.urls')),
 ]
--- NoticeBoard/accounts/views.py
+++ NoticeBoard/accounts/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RegistrationForm
+from django.contrib.auth import login
 
 # Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('notices')
+    else:
+            form = RegistrationForm()
+    return render(request, 'auth/register.html', {'form':form})
