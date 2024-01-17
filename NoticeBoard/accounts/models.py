1afc6c53df52636159e86760d0faf23af79ed8f5
jagatkhatri01 <jagatkhatri383@gmail.com>
2024-01-17 10:33:24 +0545

    auth

--- NoticeBoard/accounts/models.py
+++ NoticeBoard/accounts/models.py
@@ -1,33 +1,13 @@
+
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import AbstractUser
 
-class CustomUserManager(BaseUserManager):
-    def create_user(self, email, name, password=None, role=None, **extra_fields):
-        if not email:
-            raise ValueError('The Email field must be set')
-        email = self.normalize_email(email)
-        user = self.model(email=email, name=name, role=role, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, email, name, password=None, role=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        return self.create_user(email, name, password, role, **extra_fields)
-
-class User(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(unique=True)
+# Create your models here.
+class User(AbstractUser):
     name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
     role = models.CharField(max_length=40)
 
-    objects = CustomUserManager()
-
-    USERNAME_FIELD = 'email'  # Specify the email field as the username field
-    REQUIRED_FIELDS = ['name', 'role']
-
-    groups = models.ManyToManyField(Group, related_name='custom_user_set')
-    user_permissions = models.ManyToManyField(Permission, related_name='custom_user_set')
-
     def __str__(self):
-        return self.name
+        return self.name 
